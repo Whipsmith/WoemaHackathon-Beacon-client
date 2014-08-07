0ffc1900fb5929c1502dc553d0893691cfd9e5de
Daniel <whipsmith@gmail.com>
2014-08-07 08:38:50 +0200

    Initial commit

---
+++ src/main/java/com/example/WoemaBeacon/Home.java
@@ -0,0 +1,78 @@
+package com.example.WoemaBeacon;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class Home extends Activity {
+
+    private Handler btHandler;
+    private Handler wsHandler;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private boolean scan;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ButterKnife.inject(this);
+
+        btHandler = new Handler();
+        wsHandler = new Handler();
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @OnClick(R.id.startbutton)
+    public void startClick() {
+        scan = true;
+        initiateScan();
+    }
+
+    private void initiateScan() {
+
+    }
+
+    @OnClick(R.id.stopbutton)
+    public void stopClick() {
+        scan = false;
+    }
+}
